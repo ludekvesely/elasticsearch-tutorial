f40bae615d3eb2347f8567df6fd2d843bd556916
Luděk Veselý <ludek.vesely@email.com>
2017-09-01 01:58:17 +0200

    add commands for kibana installation

--- 01-install/install-elasticsearch.sh
+++ 01-install/install-elasticsearch.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+
+# TODO: separate to install and run scripts
+
+
+# === Elasticsearch ===
+
 # download and unzip
 wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.5.1.zip
-unzip elasticsearch-5.5.1.zip  
+unzip elasticsearch-5.5.1.zip
 cd elasticsearch-5.5.1
 
 # copy sample config
@@ -15,4 +21,23 @@ cp -r ../hunspell config/
 bin/elasticsearch-plugin install analysis-icu
 
 # run elasticsearch!
-bin/elasticsearch
+{ bin/elasticsearch & } >/dev/null 2>/dev/null
+cd ..
+
+# === Kibana ===
+
+# download end extract kibana
+wget https://artifacts.elastic.co/downloads/kibana/kibana-5.5.1-darwin-x86_64.tar.gz
+tar xzf kibana-5.5.1-darwin-x86_64.tar.gz
+cd kibana-5.5.1-darwin-x86_64
+
+# run kibana
+{ bin/kibana & } >/dev/null 2>/dev/null
+
+# === Cerebro ===
+# TODO
+
+# === Shut down ===
+echo
+read -p "Press ENTER to exit..." ENTER
+kill $(jobs -p)
