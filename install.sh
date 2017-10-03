7966519b286629200a2b8d455dd20541c74e961a
Luděk Veselý <ludek.vesely@email.com>
2017-10-03 22:35:28 +0200

    :arrow_up: update elasticsearch and kibana to 5.6.2, cerebro to 0.6.8

--- install.sh
+++ install.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
 
+# cleanup old files and dirs
+rm -rf elasticsearch-* kibana-* cerebro-*
+
+
 # === Elasticsearch ===
 
 # download and unzip
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.5.1.zip
-unzip elasticsearch-5.5.1.zip
-rm elasticsearch-5.5.1.zip
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.6.2.zip
+unzip elasticsearch-5.6.2.zip
+rm elasticsearch-5.6.2.zip
 
 # copy sample config
-cp elasticsearch.yml elasticsearch-5.5.1/config/
+cp elasticsearch.yml elasticsearch-5.6.2/config/
 
 # copy czech dictionary
-cp -r hunspell elasticsearch-5.5.1/config/
+cp -r hunspell elasticsearch-5.6.2/config/
 
 # install ICU plugin
-elasticsearch-5.5.1/bin/elasticsearch-plugin install analysis-icu
+elasticsearch-5.6.2/bin/elasticsearch-plugin install analysis-icu
 
 
 # === Kibana ===
 
 # download end extract
-wget https://artifacts.elastic.co/downloads/kibana/kibana-5.5.1-darwin-x86_64.tar.gz
-tar xzf kibana-5.5.1-darwin-x86_64.tar.gz
-rm -f kibana-5.5.1-darwin-x86_64.tar.gz
+wget https://artifacts.elastic.co/downloads/kibana/kibana-5.6.2-darwin-x86_64.tar.gz
+tar xzf kibana-5.6.2-darwin-x86_64.tar.gz
+rm -f kibana-5.6.2-darwin-x86_64.tar.gz
 
 
 # === Cerebro ===
 
 # download and extract
-wget https://github.com/lmenezes/cerebro/releases/download/v0.6.6/cerebro-0.6.6.zip
-unzip cerebro-0.6.6.zip
-rm cerebro-0.6.6.zip
+wget https://github.com/lmenezes/cerebro/releases/download/v0.6.8/cerebro-0.6.8.zip
+unzip cerebro-0.6.8.zip
+rm cerebro-0.6.8.zip
--- start.sh
+++ start.sh
@@ -3,13 +3,13 @@
 echo "Starting Elasticsearch, Kibana and Cerebro..."
 
 # start elasticsearch
-{ elasticsearch-5.5.1/bin/elasticsearch & } >/dev/null 2>/dev/null
+{ elasticsearch-5.6.2/bin/elasticsearch & } >/dev/null 2>/dev/null
 
 # start kibana
-{ kibana-5.5.1-darwin-x86_64/bin/kibana & } >/dev/null 2>/dev/null
+{ kibana-5.6.2-darwin-x86_64/bin/kibana & } >/dev/null 2>/dev/null
 
 # start cerebro
-{ cerebro-0.6.6/bin/cerebro & } >/dev/null 2>/dev/null
+{ cerebro-0.6.8/bin/cerebro & } >/dev/null 2>/dev/null
 
 read -p "Press ENTER to exit..." ENTER
 kill $(jobs -p)
